c65908d6a26041d8000f76af1743ddc4fb3fb21e
ччч <khantkhant.cm@gmail.com>
2022-08-12 16:04:37 +0630

    next.config.js change

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  typescript:{
+    ignoreBuildErrors : true,
+  },
   reactStrictMode: true,
   images: {
     domains: ['yt3.ggpht.com','lh3.googleusercontent.com'],
